a976ac3ca7b4fdc2262eff38c12d5b6a4f943c34
fernanda martinez <mmartinezg35@miumg.edu.gt>
2023-06-07 09:55:17 -0600

    Login

--- PrototipoP1EF2023/main.cpp
+++ PrototipoP1EF2023/main.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <cstdlib>
+#include<fstream>
+#include<stdlib.h>
+#include<cstdlib>
+#include<conio.h>
+#include<iomanip>
+#include "Usuario.h"
 
 using namespace std;
-
+void creditosModulo();
+void ayudaSTAFF();
 int main()
 {
-    cout << "Hello world!" << endl;
+    Usuario IngresoUsuario;
+    int numero;
+    do
+    {
+    system("cls");
+    cout<<"\t\t\t-------------------------------"<<endl;
+	cout<<"\t\t\t |   COORPORACIÓN EDUCATIVA   |"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+    cout<< " ";
+    cout<<"\t\t\t Seleccionar usuario "<<endl;
+    cout<<"\t\t\t 1). Login Administración "<<endl;
+	cout<<"\t\t\t 2). Login Alumnos "<<endl;
+	cout<<"\t\t\t 3). Salir del Sistema"<<endl;
+	cout<<"\t\t\t-------------------------------"<<endl;
+	cout<< " "<<endl;
+	cout<<"\t\t\tingrese Opcion: ";
+	cin>>numero;
+
+        switch(numero)
+        {
+            case 1:
+                IngresoUsuario.iniciarSesion();
+                break;
+            case 2:
+                IngresoUsuario.iniciarSesion();
+                break;
+            case 3:
+                break;
+
+            default:
+                cout<<"\t\t\tIngrese opcion valida"<<endl;
+                cin.get();
+        }
+    } while(numero!= 3);
+
+
     return 0;
 }
